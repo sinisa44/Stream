64ad560c46186dcd265a41d07bc62bf4db6194f5
sinisa <miskovic.sinisa44@gmail.com>
2020-12-31 13:41:19 +0100

    first

---
+++ client/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import GoogleAuth from './GoogleAuth';
+
+const Header = () => {
+    return (
+     <div className="ui secondary poining menu">
+         <Link to="/" className='item'>Streamer</Link>
+         <div className="right menu">
+            <Link to="/" className="item">
+                All Streams
+            </Link>
+            <GoogleAuth/>
+         </div>
+     </div>
+    )
+}
+
+export default Header;
